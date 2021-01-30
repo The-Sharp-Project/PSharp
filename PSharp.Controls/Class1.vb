8ca736cc97a7459c616f6b4d894e9a7440e887f7
Flo Youtube 54 <f.goussot@outlook.com>
2021-01-30 18:12:59 +0100

    Ajoutez des fichiers projet.

---
+++ PSharp.Controls/Class1.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Drawing
+
+Public Class Label
+
+    Public Property Text As String
+
+    Public Property Name As String
+
+    Public Property Location As Point
+
+    Public Property Size As Size
+
+    Public Sub New(Name As String, Optional Text As String = "")
+        Me.Name = Name
+        Me.Text = Text
+    End Sub
+
+End Class
---
+++ PSharpCompiler/Class1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Compiler
+
+    Public Const Version As Integer = 1.0
+
+    Public Function Compile(project As String)
+        Throw New NotImplementedException("Non-implémenté")
+    End Function
+
+End Class
