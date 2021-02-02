d000d9885c2b8ed5730ed3a9e74ae833d7ba90c6
Flo Youtube 54 <f.goussot@outlook.com>
2021-02-02 17:26:24 +0100

    Réglage des erreurs de verification des librairies PSharp

---
+++ CompilerConsole/FrameworkLibsUtils.vb
@@ -0,0 +1,11 @@
+﻿Public Class FrameworkLibsUtils
+
+    Class FrameworkLib
+
+        Property Name As String
+
+        Property Version As String
+
+    End Class
+
+End Class
--- CompilerConsole/Module1.vb
+++ CompilerConsole/Module1.vb
@@ -1,19 +1,33 @@
-﻿Module CompilerConsole
+﻿Imports System.Net
+Imports Newtonsoft.Json
+Imports Newtonsoft.Json.Linq
+
+Module CompilerConsole
 
     Dim WithEvents timer As New Timers.Timer(500) With {
         .AutoReset = True,
         .Enabled = False
     }
+    Dim exiting As Boolean = False
+
 
     Sub Main()
         Console.Clear()
+        Console.CursorVisible = False
         WriteHeader()
         Console.WriteLine("  ")
         Console.WriteLine("  [1] Compile project to VisualBasic.NET")
         Console.WriteLine("  [2] Compile project to C#.NET")
         Console.WriteLine("  [3] Compile single file to DLL")
         Console.WriteLine("  [4] Help about the Compiler Console")
-        Console.WriteLine("  [5] Exit Compiler Console")
+        Console.WriteLine("  [5] Check for problems with PSharp libs")
+        Console.WriteLine("  [6] Exit Compiler Console")
+
+        If exiting Then
+            Console.WriteLine("  ")
+            Console.WriteLine("  Press [6] again to exit, else press another key")
+        End If
+
 actions:
         Dim key = Console.ReadKey(True).Key
         If key = ConsoleKey.D1 Then
@@ -21,8 +35,16 @@ actions:
         ElseIf key = ConsoleKey.D4 Then
             ShowHelp()
         ElseIf key = ConsoleKey.D5 Then
-            End
+            CheckLibs()
+        ElseIf key = ConsoleKey.D6 Then
+            If exiting Then
+                End
+            Else
+                exiting = True
+                Main()
+            End If
         Else
+            exiting = False
             GoTo actions
         End If
     End Sub
@@ -56,6 +78,42 @@ actions:
         End If
     End Sub
 
+    Sub CheckLibs()
+        Console.Clear()
+        WriteHeader()
+        Console.WriteLine("  ")
+        Console.WriteLine("  [V] = OK")
+        Console.WriteLine("  [!] = Outdated")
+        Console.WriteLine("  [X] = Invalid or inexistent file")
+        Console.WriteLine("  ")
+        Console.WriteLine("  Checking PSharp Framework libs...")
+        Console.WriteLine("  ")
+        Dim jsonfile As JObject = JObject.Parse(FileIO.FileSystem.ReadAllText("frameworklibs.json"))
+        Dim list As List(Of JToken) = jsonfile("latest")("components").Children.ToList
+        For Each token As JToken In list
+            Dim obj As FrameworkLibsUtils.FrameworkLib = token.ToObject(Of FrameworkLibsUtils.FrameworkLib)
+            If FileIO.FileSystem.FileExists(obj.Name) Then
+                Dim assembly = FileVersionInfo.GetVersionInfo(obj.Name)
+                If assembly.FileVersion = obj.Version Then
+                    Console.WriteLine(String.Format("  {0} V{1} [V]", obj.Name, obj.Version))
+                Else
+                    Console.WriteLine(String.Format("  {0} V{1} [!]", obj.Name, obj.Version))
+                End If
+            Else
+                Console.WriteLine(String.Format("  {0} V{1} [X]", obj.Name, obj.Version))
+            End If
+        Next
+        Console.WriteLine("  ")
+        Console.WriteLine("  [1] Back")
+actions:
+        Dim key = Console.ReadKey(True).Key
+        If key = ConsoleKey.D1 Then
+            Main()
+        Else
+            GoTo actions
+        End If
+    End Sub
+
     Sub ShowHelp()
         Console.Clear()
         WriteHeader()
@@ -69,6 +127,11 @@ actions:
         Console.WriteLine("  [/showtrace] => Show the entire trace of the compiler")
         Console.WriteLine("  {/project='string'} => Location of the project file")
         Console.WriteLine("  [/language='vb/cs' => Compile in the specified language. Default is VB")
+        Console.WriteLine("  ")
+        Console.WriteLine("  +========== WARNING ==========+")
+        Console.WriteLine("  |     NOT YET IMPLEMENTED     |")
+        Console.WriteLine("  |         COMING SOON         |")
+        Console.WriteLine("  +=============================+")
         Console.WriteLine("  ")
         Console.WriteLine("  [1] Back")
 actions:
